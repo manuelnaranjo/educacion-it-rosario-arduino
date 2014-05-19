ae9c3bd4e018f90b1e70d98d0d1c34e4576810bd
Manuel Francisco Naranjo <mnaranjo@irobot.com>
2014-05-19 17:09:27 -0300

    Actualizando código

--- prechin_lcd_1602/prechin_lcd_1602/prechin_lcd_1602.ino
+++ prechin_lcd_1602/prechin_lcd_1602/prechin_lcd_1602.ino
@@ -6,8 +6,8 @@
  disponibles y normalmente los reconoces porque tienen una interfaz
  de 16 pines.
  
- Este sketch imprime "Hola mundo!" en la pantalla y muestra el tiempo
- desde el ultimo reinicio.
+ Este sketch imprime "Hola mundo!" en la pantalla y muestra el valor 
+ de la entrada analogica en milli volts
  
  Basado en el codigo:
  Library originally added 18 Apr 2008
@@ -27,33 +27,8 @@
 // incluir libreria:
 #include <LiquidCrystal.h>
 
-#if defined(ARDUINO) && ARDUINO >= 100
-  #include "Arduino.h"
-#else
-  #include "WProgram.h"
-#endif
-
 #define BITS_FOUR
 
-#ifdef BITS_EIGHT
-// definir un par de macros para que sea mas sencillo modificar en el futuro
-#define LCD_RS 9
-#define LCD_RW 10
-#define LCD_EN 8
-
-#define LCD_D0 0
-#define LCD_D1 1
-#define LCD_D2 2
-#define LCD_D3 3
-#define LCD_D4 4
-#define LCD_D5 5
-#define LCD_D6 6
-#define LCD_D7 7
-
-LiquidCrystal lcd(LCD_RS, LCD_RW, LCD_EN, 
-  LCD_D0, LCD_D1, LCD_D2, LCD_D3, LCD_D4, LCD_D5, LCD_D6, LCD_D7); 
-
-#elif defined BITS_FOUR
 #define LCD_RS 7
 #define LCD_EN 6
 
@@ -63,8 +38,6 @@ LiquidCrystal lcd(LCD_RS, LCD_RW, LCD_EN,
 #define LCD_D7 2
 
 LiquidCrystal lcd(LCD_RS, LCD_EN, LCD_D4, LCD_D5, LCD_D6, LCD_D7); 
-
-#endif
 
 #define LCD_COLS  16
 #define LCD_ROWS  2
@@ -81,15 +54,15 @@ void setup() {
   lcd.begin(LCD_COLS, LCD_ROWS);
   lcd.noAutoscroll();
   lcd.noCursor(); 
+  
+  //inicializar puerto serie
+  Serial.begin(115200);
 }
 
 void updateDisplay() {
   lcd.clear();
   lcd.setCursor(col, 0);
   lcd.print(texto);
-  //lcd.print(col, DEC);
-  //lcd.setCursor(10, 0);
-  //lcd.print(order, DEC);
     
   if (order == 0) {
     col++;
@@ -105,21 +78,16 @@ void updateDisplay() {
 }
 
 void loop() {
+   int sensor;
    delay(200);
    count++;
    if (count % 2 == 0)
      updateDisplay();
    lcd.setCursor(0, 1);
-   //lcd.print(millis(), HEX);
-   lcd.print(analogRead(0)*5000.0/1024);
+   sensor = analogRead(5);
+   Serial.println(sensor);
+   lcd.print(sensor*5000.0/1024);
    lcd.setCursor(6, 1);
    lcd.print("mV");
 }
-  
-  // setar el cursor a la segunda linea, columna 0
-  //lcd.setCursor(0, 1);
-  // imprimir cantidad de segundos desde el inicio
-  //lcd.print(millis()/1000);
-  
-//}
 
